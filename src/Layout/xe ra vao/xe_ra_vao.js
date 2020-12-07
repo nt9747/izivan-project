eae2d0d846c5a01cb32ee1ec082cb49ea17d2c7d
nt9747 <36836563+nt9747@users.noreply.github.com>
2020-12-07 15:27:27 +0700

    fix xe ra vao

--- src/Layout/xe ra vao/xe_ra_vao.js
+++ src/Layout/xe ra vao/xe_ra_vao.js
@@ -392,12 +392,12 @@ class Content extends React.Component {
         const token = Cookie.get("SESSION_ID");
         if (isLoading) {
             return (
-                <div style={{textAlign: 'center', marginTop: '100px'}}>
-                    <div style={{width: '50px', height: '50px'}} class="spinner-border text-primary" role="status">
+                <div style={{ textAlign: 'center', marginTop: '100px' }}>
+                    <div style={{ width: '50px', height: '50px' }} class="spinner-border text-primary" role="status">
                         <span class="sr-only">a</span>
                     </div>
                     <div>
-                        <p style={{fontSize: '20px'}}>Loading...</p>
+                        <p style={{ fontSize: '20px' }}>Loading...</p>
                     </div>
                 </div>
             )
@@ -558,7 +558,8 @@ class Content extends React.Component {
                                             </button>
                                         </form>
                                     </div>
-                                    <div class="col-2"><br />
+                                    <div class="col-4"><br />
+                                        <div style={{ marginRight: "30px" }}>
                                         <svg onClick={() => this.setState({ page: 1 }) || this.list()} width="1.7em" height="1.7em" viewBox="0 0 16 16" class="bi bi-skip-start-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M4.5 3.5A.5.5 0 0 0 4 4v8a.5.5 0 0 0 1 0V4a.5.5 0 0 0-.5-.5z" />
                                             <path d="M4.903 8.697l6.364 3.692c.54.313 1.232-.066 1.232-.697V4.308c0-.63-.692-1.01-1.232-.696L4.903 7.304a.802.802 0 0 0 0 1.393z" />
@@ -570,11 +571,21 @@ class Content extends React.Component {
                                         <svg width="1.7em" height="1.7em" onClick={() => this.listInNext()} viewBox="0 0 16 16" class="bi bi-caret-right-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
                                         </svg>
-                                        <svg onClick={() => this.setState({ page: this.state.totalPage - 1 }) || this.listInNext()} width="1.7em" height="1.7em" viewBox="0 0 16 16" class="bi bi-skip-end-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <svg onClick={() => this.listTo()} width="1.7em" height="1.7em" viewBox="0 0 16 16" class="bi bi-skip-end-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M12 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z" />
                                             <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
                                         </svg>
-
+                                        </div>
+                                        <div>
+                                            <b>Số trang </b>
+                                            <select value={this.state.limitPage} onChange={(e) => this.handleTextChange('limitPage', e) || this.setState({ page: 1 }) || this.list()}>
+                                                <option selected disabled hidden>Chọn</option>
+                                                <option value='10'>10</option>
+                                                <option value='20'>20</option>
+                                                <option value='35'>35</option>
+                                                <option value='50'>50</option>
+                                            </select>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
