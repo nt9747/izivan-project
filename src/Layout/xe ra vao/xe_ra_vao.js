f07e8306c29afe289e574e814f28f6bb05ab1c05
vu991999 <vu991999@gmail.com>
2020-12-07 15:31:02 +0700

    a

--- src/Layout/xe ra vao/xe_ra_vao.js
+++ src/Layout/xe ra vao/xe_ra_vao.js
@@ -559,7 +559,8 @@ class Content extends React.Component {
                                         </form>
                                     </div>
                                     <div class="col-4"><br />
-                                        <div style={{ marginRight: "30px" }}>
+                                        <div class="row">
+                                        <div style={{ marginRight: "90px" }}>
                                         <svg onClick={() => this.setState({ page: 1 }) || this.list()} width="1.7em" height="1.7em" viewBox="0 0 16 16" class="bi bi-skip-start-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M4.5 3.5A.5.5 0 0 0 4 4v8a.5.5 0 0 0 1 0V4a.5.5 0 0 0-.5-.5z" />
                                             <path d="M4.903 8.697l6.364 3.692c.54.313 1.232-.066 1.232-.697V4.308c0-.63-.692-1.01-1.232-.696L4.903 7.304a.802.802 0 0 0 0 1.393z" />
@@ -585,6 +586,7 @@ class Content extends React.Component {
                                                 <option value='35'>35</option>
                                                 <option value='50'>50</option>
                                             </select>
+                                        </div>
                                         </div>
                                     </div>
                                 </div>
